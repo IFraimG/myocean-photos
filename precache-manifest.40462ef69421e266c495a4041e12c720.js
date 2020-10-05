20dd2ab54926ffcfd5c0a651e67e38be1a9cdc05
IFrame <san4uk777@gmail.com>
2020-10-05 23:03:25 +0300

    Фикс багов

---
+++ precache-manifest.40462ef69421e266c495a4041e12c720.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d33818c478fa872dac",
+    "url": "/css/app.2c3a7404.css"
+  },
+  {
+    "revision": "f2abad5df90b8cca8201",
+    "url": "/css/chunk-vendors.84bb20f7.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "1212787bbc09af41612c0e8be80a214d",
+    "url": "/img/error.1212787b.jpg"
+  },
+  {
+    "revision": "ba07b64dc9724ee59f3166cc1b77b6e9",
+    "url": "/img/logo.ba07b64d.svg"
+  },
+  {
+    "revision": "b7f713acfbf0bda369a005d95921e2de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39d33818c478fa872dac",
+    "url": "/js/app.c1b1a5c0.js"
+  },
+  {
+    "revision": "f2abad5df90b8cca8201",
+    "url": "/js/chunk-vendors.2d600592.js"
+  },
+  {
+    "revision": "bd343b4cebe59df903c4797a48299346",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e07160b0dac2d818d837bc03f16a66b2.js"
+  "/precache-manifest.40462ef69421e266c495a4041e12c720.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "junior-project"});
